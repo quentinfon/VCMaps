1e271780656255025b536194926512d5560350a9
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-07-16 15:00:38 +0200

    V1 affichage

---
+++ app/src/main/java/fr/qfondev/vcmaps/vue/MenuPrincipalActivity.java
@@ -0,0 +1,23 @@
+package fr.qfondev.vcmaps.vue;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.qfondev.vcmaps.R;
+
+public class MenuPrincipalActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+
+}
---
+++ app/src/main/java/fr/qfondev/vcmaps/vue/StartingActivity.java
@@ -0,0 +1,31 @@
+package fr.qfondev.vcmaps.vue;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.qfondev.vcmaps.R;
+
+public class StartingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_starting);
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(getApplicationContext(),MenuPrincipalActivity.class);
+                startActivity(main);
+                finish();
+            }
+
+        };
+
+        new Handler().postDelayed(runnable,2000);
+    }
+}
